2e68f330998bd508209e6c371026b0cf51bd0537
kirre02 <kirre02@outlook.com>
2024-10-31 22:28:39 +0100

    test: Created test for checkWinner function

--- src/test/java/org/example/tictactoe/BoardTest.java
+++ src/test/java/org/example/tictactoe/BoardTest.java
@@ -32,5 +32,68 @@ class BoardTest {
         assertEquals('X', board.getCell(0, 0), "Cell (0,0) should remain marked with 'X'");
     }
 
+    @Test
+    void testRowWin() {
+        // Make moves to complete the first row with 'X' (current player)
+        board.makeMove(0, 0);  // X
+        board.makeMove(1, 0);  // O
+        board.makeMove(0, 1);  // X
+        board.makeMove(1, 1);  // O
+        board.makeMove(0, 2);  // X
+
+        assertTrue(board.checkWinner(), "Player X should win with a complete row");
+    }
+
+    @Test
+    void testColumnWin() {
+        // Make moves to complete the first column with 'X' (current player)
+        board.makeMove(0, 0);  // X
+        board.makeMove(0, 1);  // O
+        board.makeMove(1, 0);  // X
+        board.makeMove(1, 1);  // O
+        board.makeMove(2, 0);  // X
+
+        assertTrue(board.checkWinner(), "Player X should win with a complete column");
+    }
+
+    @Test
+    void testDiagonalWin() {
+        // Make moves to complete the diagonal from top-left to bottom-right with 'X'
+        board.makeMove(0, 0);  // X
+        board.makeMove(0, 1);  // O
+        board.makeMove(1, 1);  // X
+        board.makeMove(0, 2);  // O
+        board.makeMove(2, 2);  // X
+
+        assertTrue(board.checkWinner(), "Player X should win with a diagonal from top-left to bottom-right");
+    }
+
+    @Test
+    void testOtherDiagonalWin() {
+        // Make moves to complete the anti-diagonal from top-right to bottom-left with 'X'
+        board.makeMove(0, 2);  // X
+        board.makeMove(0, 1);  // O
+        board.makeMove(1, 1);  // X
+        board.makeMove(1, 0);  // O
+        board.makeMove(2, 0);  // X
+
+        assertTrue(board.checkWinner(), "Player X should win with a diagonal from top-right to bottom-left");
+    }
+
+    @Test
+    void testNoWin() {
+        board.makeMove(0, 0);  // X
+        board.makeMove(0, 1);  // O
+        board.makeMove(0, 2);  // X
+        board.makeMove(1, 0);  // O
+        board.makeMove(1, 2);  // X
+        board.makeMove(2, 1);  // O
+
+        assertFalse(board.checkWinner(), "There should be no winner with this setup");
+    }
+
+
+
+
 
 }
